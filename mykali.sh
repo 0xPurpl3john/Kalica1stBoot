4906cf0f73cbd9ff82d880ce2fd4df8cbc23b03c
jardim <105323180+0xPurpl3john@users.noreply.github.com>
2024-03-25 21:46:45 -0300

    Update mykali.sh

--- mykali.sh
+++ mykali.sh
@@ -15,24 +15,59 @@ main() {
     echo '                           ░                     '
     echo
 
-    update_repositories
-    install_programs
-    change_background
-    disable_ipv6
-    configure_terminal
-    configure_flameshot
-    wordlist_tasks
-    # add_user
-    customize_prompt
-    restart_system
+    while true; do
+        echo "Escolha uma opção:"
+        echo "1) Atualizar repositórios"
+        echo "2) Instalar programas"
+        echo "3) Alterar papel de parede"
+        echo "4) Desabilitar IPv6"
+        echo "5) Configurar terminal"
+        echo "6) Configurar o Flameshot"
+        echo "7) Tarefas de lista de palavras"
+        # echo "8) Adicionar usuário"
+        echo "9) Personalizar o prompt"
+        echo "10) Reiniciar o sistema"
+        echo "11) Sair do programa"
+
+        read -p "Opção selecionada: " opcao
+
+        case $opcao in
+            1) update_repositories ;;
+            2) install_programs ;;
+            3) change_background ;;
+            4) disable_ipv6 ;;
+            5) configure_terminal ;;
+            6) configure_flameshot ;;
+            7) wordlist_tasks ;;
+            # 8) add_user ;;
+            9) customize_prompt ;;
+            10) restart_system ;;
+            11) echo "Saindo do programa."; exit ;;
+            *) echo "Opção inválida. Por favor, selecione uma opção válida." ;;
+        esac
+
+        read -p "Deseja realizar outra operação? (y/n): " continuar
+        if [ "$continuar" != "y" ]; then
+            break
+        fi
+    done
+    
 }
 
 update_repositories() {
+    echo '# Update repositories'
+    echo '# Update repositories'
+    echo '# Update repositories'
+
     sudo apt update
     sudo apt upgrade
 }
 
 install_programs() {
+    echo '# Instalando aplicativos'
+    echo '# Instalando aplicativos'
+    echo '# Instalando aplicativos'
+
     echo ' [+] Instalando flameshot, vim, tilix, tmux e zsh..'
     sudo apt install -y flameshot vim tilix tmux zsh
     echo
@@ -82,6 +117,10 @@ install_programs() {
 }
 
 wordlist_tasks() {
+    echo '# Configurando WORDLISTS'
+    echo '# Configurando WORDLISTS'
+    echo '# Configurando WORDLISTS'
+
     echo ' [+] Descompactando rockyou.txt e convertendo em UTF-8...'
     gunzip /usr/share/wordlists/rockyou.txt.gz
     iconv -f ISO-8859-1 -t UTF-8 /usr/share/wordlists/rockyou.txt > /usr/share/wordlists/rockyou_utf8.txt
@@ -98,23 +137,39 @@ wordlist_tasks() {
 }
 
 change_background() {
+    echo '# Configurando background'
+    echo '# Configurando background'
+    echo '# Configurando background'
+
     # gsettings set org.gnome.desktop.background picture-uri file:///caminho/para/sua/imagem.jpg
     gsettings set org.gnome.desktop.background primary-color '#000000'
 
 }
 
 disable_ipv6() {
+    echo '# Configurando IPV6'
+    echo '# Configurando IPV6'
+    echo '# Configurando IPV6'
+    
     sudo sh -c 'echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf'
     sudo sh -c 'echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf'
     sudo sysctl -p
 }
 
 configure_terminal() {
+    echo '# Configurando terminal'
+    echo '# Configurando terminal'
+    echo '# Configurando terminal'
+
     sudo update-alternatives --config x-terminal-emulator
     # Selecione o número correspondente ao tilix na lista e pressione Enter
 }
 
 configure_flameshot() {
+    echo '# Configurando flameshot'
+    echo '# Configurando flameshot'
+    echo '# Configurando flameshot'
+
     gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot "['Print']"
     gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
@@ -129,6 +184,10 @@ configure_flameshot() {
 # }
 
 customize_prompt() {
+    echo '# Customizando prompt'
+    echo '# Customizando prompt'
+    echo '# Customizando prompt'
+
     # echo 'PS1="%B%F{magenta}%n%f%b %F{158}%~:%f "' >> ~/.zshrc
     echo ' [+] Personalizando aliases no ~/.zshrc...'
 
@@ -173,18 +232,15 @@ customize_prompt() {
 }
 
 restart_system() {
-    while true; do
-        read -p "## Reiniciar sistema agora? (y/n): " resposta
-        if [ "$resposta" == "y" ]; then
-            echo "Reiniciando..."
-            sudo reboot
-        elif [ "$resposta" == "n" ]; then
-            echo "Saindo..."
-            exit 0
-        else
-            echo "Resposta invalida, insira apenas y ou n"
-        fi
-    done
+    read -p "## Reiniciar sistema agora? (y/n): " resposta
+    if [ "$resposta" == "y" ]; then
+        echo "Reiniciando..."
+        sudo reboot
+    elif [ "$resposta" == "n" ]; then
+        echo "Saindo..."
+    else
+        echo "Resposta invalida, insira apenas y ou n"
+    fi
 }
 
 main
