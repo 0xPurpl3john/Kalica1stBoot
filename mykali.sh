18ca329b1a769cd5e3b3043fd540e96868a923dd
jardim <105323180+0xPurpl3john@users.noreply.github.com>
2024-03-25 21:30:18 -0300

    Update mykali.sh

--- mykali.sh
+++ mykali.sh
@@ -180,8 +180,11 @@ restart_system() {
             sudo reboot
         elif [ "$resposta" == "n" ]; then
             echo "Saindo..."
+            exit 0
         else
             echo "Resposta invalida, insira apenas 'y' ou 'n'"
+        fi
+    done
 }
 
 main
